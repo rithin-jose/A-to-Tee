a8d412ded45af4711d86e169015419feb43386a8
unknown <rithinja@gmail.com>
2021-06-05 22:26:02 +0530

    Update: User Schema done

---
+++ Backend/models/user.js
@@ -0,0 +1,45 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    first_name:{
+        type: String,
+        required:true,
+        trim:true
+    },
+    last_name:{
+        type: String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type: String,
+        required:true,
+        trim:true,
+        unique:true
+    },
+
+    userInfo:{
+        type: String,
+        trim:true
+    },
+
+    // TODO: Password validation
+    password:{
+        type: String,
+        trim:true
+    },
+
+    salt: String,
+    role:{
+        type:Number,
+        default:0
+    },
+    purchases:{
+        type: Array,
+        default:[]
+    }
+});
+
+// Export in node js
+module.export = mongoose.model("User",userSchema)

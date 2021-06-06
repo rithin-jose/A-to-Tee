4b7b13b1d60a69146a6632779501d88d181f620c
unknown <rithinja@gmail.com>
2021-06-06 15:11:06 +0530

    Update: User schema done

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const app = express();
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
 
+const app = express();
 const PORT = 8000;
 
 app.get("/",(req,res) => {
@@ -9,4 +10,5 @@ app.get("/",(req,res) => {
 
 app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
+    console.log(uuidv4());
 })

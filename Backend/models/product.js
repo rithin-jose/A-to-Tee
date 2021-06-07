5d394b0c6be0de68dade0bff9656116603705372
unknown <rithinja@gmail.com>
2021-06-07 11:07:32 +0530

    Update: Product Schema

---
+++ Backend/models/product.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+import {ObjectId} from mongoose.Schema
+
+var Schema  = mongoose.Schema
+
+var productSchema = new Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true,
+        maxlength:32
+    },
+    description:{
+        type:String,
+        trim:true,
+        required:true,
+        maxlength:1000,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    category:{
+        type: ObjectId,
+        ref: "Category",
+        required:true
+    },
+    stock:{
+        type:Number
+    },
+    sold:{
+        type:Number,
+        defult:0
+    },
+    photo:{
+        data: Buffer,
+        contentType: String
+    }
+
+},{timestamps:true})
+
+module.exports = mongoose.model("Product",productSchema)

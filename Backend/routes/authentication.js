b4d21e99de28df1cbf1280d669ea626039fac67a
unknown <rithinja@gmail.com>
2021-06-08 20:33:59 +0530

    Feature: validations added   - Error message not added   - User Validation only added

--- Backend/routes/authentication.js
+++ Backend/routes/authentication.js
@@ -1,9 +1,15 @@
 var express = require('express')
+const { check } = require('express-validator');
 const {signOut,signUp} = require('../controllers/authentication')
 
 var router = express.Router();
 
-router.post("/signup",signUp)
+router.post("/signup",[
+    check("first_name").isLength({min:3}).withMessage("Name should be atleast 3 character"),
+    check("email").isEmail().withMessage("Email is required"),
+    check("password").isLength({min:5}).withMessage("Password must be atleast 5 characters"),
+],signUp)
+
 router.get("/signout",signOut)
 
 module.exports = router;

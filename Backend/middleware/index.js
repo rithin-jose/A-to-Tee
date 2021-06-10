46e4f3c2b1dce0103aa20c7a96f9024a9aa9d7ed
unknown <rithinja@gmail.com>
2021-06-10 22:08:52 +0530

    Update: middlewares

--- Backend/middleware/index.js
+++ Backend/middleware/index.js
@@ -11,4 +11,23 @@ exports.isSignedIn = expressJwt({
     algorithms:['HS256']
 })
 
-// exports.isAuthenticated
+
+exports.isAuthenticated = (req,res,next) => {
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id
+
+    if(!checker){
+        res.status(403).json({
+            error:"Access Denied"
+        })
+    }
+    next();
+}
+
+exports.isAdmin = (req,res,next) => {
+    if(req.profile.role === 0){
+        return res.status(403).json({
+            error:"You are not an admin, Access Denied"
+        })
+    }
+    next();
+}
